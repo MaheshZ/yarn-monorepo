082c219d2b1de8051f11e4cef7b031b7a397b6da
Mahesh Sawaiker <maheshsa@us.ibm.com>
2020-09-05 12:06:55 +0530

    added sass support and imported styles.

--- packages/components/src/nav/index.js
+++ packages/components/src/nav/index.js
@@ -1,10 +1,12 @@
 
 import React from "react"
-import './nav.css'
+import './nav.scss'
 const WombatNav = props => {
     return (<nav className='nav'>
+        <div>Hot New Product!!</div>
           <div>Home</div>  
           <div>Profile</div>
+          <div>Docs</div>
           <div>Search <input type='textbox'/> </div>
           <div>Logout</div>  
       </nav>)
